bbdd692684915b961544574131da23709b5aa210
mogwai <feelirabs@gmail.com>
2020-09-03 03:25:57 +0100

    all endpoints protected

--- server.js
+++ server.js
@@ -3,23 +3,32 @@ const express = require('express')
 let bcrypt = require('bcryptjs')
 let salt = bcrypt.genSaltSync(14);
 let bodyParser = require('body-parser')
-let jwt = require('jsonwebtoken')
 let generateToken = require('./utils/generateToken');
+let cookieParser = require('cookie-parser');
+let token;
 
 //dabatase helpers
 const User = require('./data/dbhelpers');
 
-//jwt secret
-const secret = require('./utils/secrets')
+
 
 //set up server and port
 const server = express();
 server.use(bodyParser.json())
+server.use(cookieParser())
 const PORT = process.env.PORT || 5000;
 
 // Fetch users. To-do: protect this endpoint
 server.get('/', (req, res)=>{
-    User.find().then(users=> res.status(200).send(users))
+    if(res.cookie){
+        if(req.cookies.auth){
+    User.find().then(users=> res.status(200).json({users: users}))
+}else {
+    res.status(401).json({message: `You're not cleared for this.`})
+}
+} else {
+    res.status(401).json({message: `You shouldn't be here`})
+}
 })
 
 //Registration endpoint
@@ -35,7 +44,8 @@ server.post('/login', (req, res)=> {
     User.findbyUsername(username)
         .then(user => {
             if(user && bcrypt.compareSync(password, user.password)) {
-                const token = generateToken(user)
+                token = generateToken(user)
+                res.cookie('auth', token);
                 res.status(200).json({message: `Welcome ${user.username}! Here's your token:`, token})
             }
             else {
@@ -45,10 +55,15 @@ server.post('/login', (req, res)=> {
         .catch(err=> res.json({error: err.message}))
 })
 
-server.get('/:id', (req, res)=> {
-    const id = req.params.id;
-    User.findbyid(id).then(user=>res.status(200).send(user))
+server.get('/logout', (req, res)=> {
+    res.clearCookie('auth', token)
+    res.status(200).json({message: `You're now logged out`})
 })
+
+// server.get('/:id', (req, res)=> {
+//     const id = req.params.id;
+//     User.findbyid(id).then(user=>res.status(200).send(user))
+// })
 
 server.listen(PORT, ()=> {
     console.log(`Server is now active on localhost:${PORT}`)
