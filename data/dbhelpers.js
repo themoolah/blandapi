a70e87e53e081b08ae89ca7e132399bf19b59d70
mogwai <feelirabs@gmail.com>
2020-09-14 20:35:16 +0100

    restored silly backend bullshit

--- data/dbhelpers.js
+++ data/dbhelpers.js
@@ -4,9 +4,7 @@ const db = require('../db');
 
 function findbyUsername(username){
     return db('users')
-        .join('wants', 'users.id', '=', 'wants.user_id')
-        .join('needs', 'users.id', '=', 'needs.user_id')
-        // .where({'users.username': 'vundie'})
+            .where({'users.username': 'vundie'})
 
 }
 
